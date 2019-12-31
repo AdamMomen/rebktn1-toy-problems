ac12726b134ea2b3065701c527db4420cd375d77
AdamMomen <adammuman81@gmail.com>
2019-12-31 09:34:31 +0100

    ProblemSovled

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -4,6 +4,19 @@ Given an array of negative/positive integers, return true if there exists two nu
 
 Constraints: Solve this linear time complexity O(n)
 
-zeroSum([ 1, 3, 2, -3 ]) // true
+([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+//input => array of numbers
+//output => boolean
+
+function zeroSum(array) {
+    var elementObj = {}
+    array.forEach(element => { elementObj[element] = element })
+    for (var key in elementObj) {
+        if (elementObj[-(JSON.parse(key))]) {
+            return true
+        }
+    }
+    return false
+}

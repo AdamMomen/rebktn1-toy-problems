c41e50cdc11407766bedb2d8ef1fb48d0535d7c1
AdamMomen <adammuman81@gmail.com>
2020-01-03 09:41:55 +0100

    in progress

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -11,5 +11,48 @@ longestRun("mississippi") // [ 2, 3 ]
 longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
+// i/p => string 
+// o/p => array of numbers  > 0
 
-function longestRun(string) {}
+
+//naive solution 
+
+function longestRun(string) {
+    debugger;
+    //declare an object
+    var charData = {
+    }
+    //iterate + store in the object
+    for (var i = 0; i < string.length; i++) {
+        const index = i;
+        var element = string[i];
+        //next element is the same
+        if (element === string[i + 1]) {
+            if (!charData[element]) {
+                let item = {
+                    freq: 1,
+                    index: index,
+                    previous: [],
+                }
+                charData[element] = item;
+            } else {
+                var pushed = charData[element].previous.push(index)
+                let item = {
+                    index: i,
+                    previous: pushed,
+                    freq: charData[element].freq + 1,
+                };
+                charData[element] = item
+            }
+        }
+    }
+    //check the largest one
+    for (var key in charData) {
+        var max = 0;
+        if (key.freq > max) {
+            max = key.freq
+        }
+    }
+    // return the indecies array
+    return max.previous;
+}

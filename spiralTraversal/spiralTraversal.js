093244bb07ebfa92731284f2f718e9861a19504f
AdamMomen <adammuman81@gmail.com>
2020-01-13 09:47:46 +0100

    inProgress

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -42,3 +42,69 @@ spiralTraversal([[ 1 ], [ 2 ], [ 3 ], [ 4 ]])	// [ 1, 2, 3, 4 ]
 
 spiralTraversal([[ 1, 2, 3, 4, 5, 6, 7 ]]) // [ 1, 2, 3, 4, 5, 6, 7 ]
 */
+
+// @params MainArray = []
+var spiralTraversal = (MainArray)=>{
+}
+// assign an object for dictionary
+
+var traverseRight = (row) => {
+	var resultArr = [];
+for (var i = 0; i < row.length; i++) {
+		resultArr.push(MainArray[i])
+	}
+	return resultArr
+}
+var traverseDown = (row, column)=>{
+		var resultArr = []
+for (var i = 0; i < MainArray.length; i++) {
+		resultArr.push(row[row][i])
+	}
+	return resultArr
+}
+var traverseLeft = (row, column)=>{
+		var resultArr = []
+for (var i = MainArray.length; i >= 0; i--) {
+		resultArr.push(row[i])
+	}
+	return resultArr
+}
+
+var traverseUp = (row, column)=>{
+	var resultArr = []
+for (var i = MainArray.length; i >= 0 ; i++) {
+		resultArr.push(MainArray[row][i])
+	}
+	return resultArr
+}
+
+///
+spiralArray = function (edge) {
+    var arr = Array(edge),
+        x     = 0, 
+        y     = edge,
+        total = edge * edge--,
+        dx    = 1, 
+        dy    = 0,
+        i     = 0, 
+        j     = 0;
+
+    while (y) arr[--y] = [];
+    while (i < total) {
+        arr[y][x] = i++;
+        x += dx; y += dy;
+        if (++j == edge) {
+            if (dy < 0) {
+            	x++; 
+            	y++; 
+            	edge -= 2
+            }
+            j = dx; dx = -dy; dy = j; j = 0;
+       }
+    }
+    return arr;
+}
+ 
+// T E S T:
+arr = spiralArray(edge = 5);
+for (y= 0; y < edge; y++) console.log(arr[y].join(" "));

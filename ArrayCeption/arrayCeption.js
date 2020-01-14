7c377d109da65da2c02497002ff77d9985fdddc9
AdamMomen <adammuman81@gmail.com>
2020-01-14 10:01:27 +0100

    inprogress

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -5,7 +5,7 @@
 
 // Examples
 //  Input 	Output
-//  array:
+//  array: 
 //  [ [ 5 ], [ [ ] ] ] ==>	2
 //  array:
 //  [ 10, 20, 30, 40 ] ==>	1
@@ -15,3 +15,32 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+
+//********************
+//NAIVE SOLUTION 
+//********************
+
+//input array
+//output number
+
+// pseudoCode
+
+/////
+
+//recurions approach
+//for iteration is the base case stopper
+//
+
+
+function arrayception(arrayOfArrays, depth) {
+    //          Eject mechanism
+    if (arrayOfArrays[i]) {
+        return false;
+    }
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        return arrayception(arrayOfArrays[i], depth + 1)
+    }
+    if (!arrayception) {
+        return depth;
+    }
+}

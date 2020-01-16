3510a82551c9741fc7bb48ad4244547591763bb1
AdamMomen <adammuman81@gmail.com>
2020-01-16 09:34:20 +0100

    inprogress

--- countLeaves/countLeaves.js
+++ countLeaves/countLeaves.js
@@ -29,11 +29,30 @@ root.countLeaves(); // 3
 
 */
 
-var Tree = function(value) {
+var Tree = function (value) {
   this.value = value;
   this.children = [];
 };
 
+
+///////////////////////////////////
+Tree.prototype.countLeaves = function () {
+  var counter = 0;
+  if (!this.children.length) {
+    counter += 1;
+
+  } else {
+
+    for (let i = 0; i < this.children.length; i++) {
+      counter = this.children[i].countLeaves()
+    }
+  }
+  return counter;
+}
+
+
+
+//////////////////////////////////////
 /**
  * You shouldn't need to change anything below here, but feel free to look.
  */
@@ -42,7 +61,7 @@ var Tree = function(value) {
  * add an immediate child
  * (wrap values in Tree nodes if they're not already)
  */
-Tree.prototype.addChild = function(child) {
+Tree.prototype.addChild = function (child) {
   if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
@@ -60,7 +79,7 @@ Tree.prototype.addChild = function(child) {
  * check to see if the provided tree is already a child of this
  * tree __or any of its sub trees__
  */
-Tree.prototype.isDescendant = function(child) {
+Tree.prototype.isDescendant = function (child) {
   if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
@@ -78,7 +97,7 @@ Tree.prototype.isDescendant = function(child) {
 /**
  * remove an immediate child
  */
-Tree.prototype.removeChild = function(child) {
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
   if (index !== -1) {
     // remove the child
--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,4 +9,15 @@ primeTester(1) // false
 
 */
 
-function primeTester(n) {}
+function primeTester(n) {
+  if (n <= 0 || typeof (n) !== 'number') return;
+  var prime = false;
+  for (let i = 1; i <= n; i++) {
+    if (i !== 1 && i !== n) {
+      if (n % i === 0) {
+        return false
+      }
+    }
+  }
+  return prime
+}

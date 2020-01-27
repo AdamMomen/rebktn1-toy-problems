3943e243d395c60efdb822856208c20fc7aedb3c
AdamMomen <adammuman81@gmail.com>
2020-01-27 09:59:03 +0100

    problem Solved

--- linkedListCycles/cycles.js
+++ linkedListCycles/cycles.js
@@ -27,6 +27,38 @@ function Node(val) {
   return obj;
 }
 
+// First solution
+//pseudocode
+
 var hasCycle = function(linkedList) {
-  // your code here...
+  debugger;
+  // pointers
+  let p = linkedList;
+  let q = linkedList.next.next;
+  //traverse
+  while (p) {
+    if (p == q) {
+      return true;
+    }
+    console.log("p", p, "q", q);
+    p = p.next;
+    q = q.next;
+  }
 };
+//maybe I need to change the const to var later
+const linked = new Node("A");
+const cycled = (linked.next = new Node("B"));
+linked.next.next = new Node("C");
+linked.next.next.next = new Node("D");
+const pre_cycled = (linked.next.next.next.next = new Node("E"));
+pre_cycled.next = cycled;
+hasCycle(linked);
+// second solution
+//pseudocode
+// 0 declare a memo
+// need to check if we have
+// 1 check if node is visited in memo
+// add it
+// 2 traverse
+// else
+//rerurn true

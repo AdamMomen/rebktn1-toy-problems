e29ce981dc9aa25ca07f48a230bb6597e27fc117
AdamMomen <adammuman81@gmail.com>
2020-01-28 10:13:26 +0100

    in progress

--- powerSet/index.js
+++ powerSet/index.js
@@ -21,47 +21,54 @@ powerSet("horse") // [
                     ]
 */
 //super naive solution
-let charbj = {
-  0: "a",
-  1: "b",
-  2: "c",
-  3: "d",
-  4: "e",
-  5: "f",
-  6: "g",
-  7: "h",
-  8: "i",
-  9: "j",
-  10: "k",
-  11: "l",
-  12: "m",
-  13: "n",
-  14: "o",
-  15: "p",
-  16: "q",
-  17: "r",
-  18: "s",
-  19: "t",
-  20: "u",
-  21: "v",
-  22: "w",
-  23: "x",
-  24: "y",
-  25: "z"
-};
+// let charbj = {
+//   0: "a",
+//   1: "b",
+//   2: "c",
+//   3: "d",
+//   4: "e",
+//   5: "f",
+//   6: "g",
+//   7: "h",
+//   8: "i",
+//   9: "j",
+//   10: "k",
+//   11: "l",
+//   12: "m",
+//   13: "n",
+//   14: "o",
+//   15: "p",
+//   16: "q",
+//   17: "r",
+//   18: "s",
+//   19: "t",
+//   20: "u",
+//   21: "v",
+//   22: "w",
+//   23: "x",
+//   24: "y",
+//   25: "z"
+// };
 
 function powerSet(string) {
-  const chars = new Set("");
-  string
-    .slice()
-    .split("")
-    .forEach(char => {
-      if (!chars.has(char)) {
-        chars.add(char);
-      }
-    });
-  let temp = console.log(Object.keys(chars));
-  return temp;
+  let chars = new Set("");
+  //sort it
+  // string.split("").sort.join(" ");
+  //get a random number
+  while (chars.length < Math.pow(string.length, string.length) - 1) {
+    let start = randomGenarator(string.length);
+    let end = randomGenarator(string.length);
+    let str = substring(start, end);
+    console.log(str);
+    if (!chars.has(str)) {
+      console.log("heys");
+      chars.add(str);
+    }
+  }
+  return console.log(chars.values());
+}
+function randomGenarator(max) {
+  return Math.floor(Math.random() * Math.floor(limit));
 }
 powerSet("ab");
 // input string

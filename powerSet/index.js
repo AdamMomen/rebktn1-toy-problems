8f6958f8c2b8c81eb241183a7572fb8f560edb73
AdamMomen <adammuman81@gmail.com>
2020-01-28 09:49:52 +0100

    inprogress

--- powerSet/index.js
+++ powerSet/index.js
@@ -13,11 +13,68 @@ Make sure your code does the following:
 Example: 
 
 powerSet("a") // [ "", "a" ]
-powerSet("ab") // [ "", "a", "ab", "b" ]
+powerSet("ab") // [ "", "a", "ab", "b" ] => duplicates
 powerSet("obama") // [ "", "a", "ab", "abm", "abmo", "abo", "am", "amo", "ao", "b", "bm", "bmo", "bo", "m", "mo", "o" ]
-powerSet("horse") // [ "", "e", "eh", "eho", "ehor", "ehors", "ehos", "ehr", "ehrs", "ehs", "eo", "eor", "eors", "eos", "er", "ers", "es", "h", "ho", "hor", "hors", "hos", "hr", "hrs", "hs", "o", "or", "ors", "os", "r", "rs", "s" ]
+powerSet("horse") // [ 
+  "", "e", "eh", "eho", "ehor", "ehors", "ehos", "ehr", "ehrs", "ehs", "eo", "eor", "eors", "eos", "er", "ers",
+  "es", "h", "ho", "hor", "hors", "hos", "hr", "hrs", "hs", "o", "or", "ors", "os", "r", "rs", "s" 
+                    ]
 */
+//super naive solution
+let charbj = {
+  0: "a",
+  1: "b",
+  2: "c",
+  3: "d",
+  4: "e",
+  5: "f",
+  6: "g",
+  7: "h",
+  8: "i",
+  9: "j",
+  10: "k",
+  11: "l",
+  12: "m",
+  13: "n",
+  14: "o",
+  15: "p",
+  16: "q",
+  17: "r",
+  18: "s",
+  19: "t",
+  20: "u",
+  21: "v",
+  22: "w",
+  23: "x",
+  24: "y",
+  25: "z"
+};
 
 function powerSet(string) {
-  // your code here...
+  const chars = new Set("");
+  string
+    .slice()
+    .split("")
+    .forEach(char => {
+      if (!chars.has(char)) {
+        chars.add(char);
+      }
+    });
+  let temp = console.log(Object.keys(chars));
+  return temp;
 }
+powerSet("ab");
+// input string
+// output array of strings
+// edge cases => not a string
+// constraits => none
+
+//notes
+//1 using set
+
+//challenges
+//shuffling words/
+
+//basic steps must done
+// split
+// iterate

de68467ae6d353c37b2a7fca867f6b1e0dcc6dab
AdamMomen <adammuman81@gmail.com>
2020-01-31 10:08:11 +0100

    inprogress

--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -8,19 +8,37 @@ Output: 'solved' if the board is valid, 'invalid' if it isn't
 
 Example input: 
 "735814296\n
-896275314\n
-214963857\n
-589427163\n
-362189745\n
-471356982\n
-923541678\n
-648792531\n
-157638429"
+ 896275314\n
+ 214963857\n
+ 589427163\n
+ 362189745\n
+ 471356982\n
+ 923541678\n
+ 648792531\n
+ 157638429"
 */
+//input string
+//output sting
+//constants none
+//edge cases none
 
+//split the board 2d array
+const stringBoard = "157638449";
+// //\n896275314\n214963857\n589427163\n362189745\n471356982\n923541678\n648792531\n157638429";
 
-
-function sudokuChecker(board) {
-  // Your code here.
-  
+function sudokuChecker(stringBoard) {
+  let sudokuObj = {};
+  let result = "solved";
+  let board = stringBoard.split("");
+  for (let i = 0; i < board.length; i++) {
+    if (sudokuObj[board[i]]) {
+      result = "invalid";
+      console.log(result);
+      return result;
+    }
+    sudokuObj[board[i]] = [board[i]];
+  }
+  console.log(result);
+  return result;
 }
+sudokuChecker(stringBoard);

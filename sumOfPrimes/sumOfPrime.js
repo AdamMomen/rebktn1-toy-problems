adc90e00205842a25af4cbaa59512af2854339db
AdamMomen <adammuman81@gmail.com>
2020-02-03 09:28:27 +0100

    solved

--- sumOfPrimes/sumOfPrime.js
+++ sumOfPrimes/sumOfPrime.js
@@ -8,9 +8,12 @@ Given a positive number, return the sum of all positive primes that are less tha
 // Constraints: none.
 // EdgeCase: postive numbers only.
 
+// Time complexity: O(n)
+// Space Complexity: O(n)
 function sumOfPrimes(n) {
   let result = 0;
   while (n > 0) {
+    // the exponential of number is for the next values
     if (Math.pow(2, n - 1) % n === 1) {
       result += n;
     }

359deaf982bf464ce2eaac7bb4f88a4e52f42a0f
AdamMomen <adammuman81@gmail.com>
2020-02-05 09:53:18 +0100

    problem sovlved

--- flattenArray/flatten.js
+++ flattenArray/flatten.js
@@ -6,7 +6,23 @@ Hint: Use Array.isArray(elem) to see if something is an array.
 Example: 
 flatten([[1, 1], [4], 3, [3], 4, [6, 7]]) // [1, 1, 4, 3, 3, 4, 6, 7]
 */
-
+//naive solution
 function flatten(arr) {
-  // your code here...
+  console.log(arr.flat());
 }
+
+function flatMeIfyouCan(arr) {
+  function flatHelper(array = [], index = 0) {
+    if (index === arr.length) {
+      return array;
+    }
+    if (Array.isArray(arr[index])) {
+      array = flatHelper(array.concat(...arr[index]), index + 1);
+    } else {
+      array = flatHelper(array.concat(arr[index]), index + 1);
+    }
+    return array;
+  }
+  console.log(flatHelper());
+}
+flatMeIfyouCan([[1, 1], [4], 3, [3], 4, [6, 7]]); // [1, 1, 4, 3, 3, 4, 6, 7]

5f346ba6990938621c76c7ee1e9e8c7f67b24ed2
AdamMomen <adammuman81@gmail.com>
2020-02-17 09:47:06 +0100

    in progress

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -5,23 +5,59 @@ Write a stack. Once you’re done, implement a queue using two stacks to create 
 The queue should not have any storage separate from its stacks.
 */
 
-var Stack = function() {
+var Stack = function () {
   var storage = [];
 
-  this.push = function() {};
+  this.push = function (val) {
+    storage.push(val)
+  };
 
-  this.pop = function() {};
+  this.pop = function () {
+    storage.pop();
+  };
 
-  this.size = function() {};
+  this.size = function () {
+    return storage.length
+  };
+
+  this.reverse = function () {
+    return storage = storage.reverse()
+  }
+
+  this.storage = function () {
+    return storage
+  }
 };
 
-var Queue = function() {
+var Queue = function () {
   var inbox = new Stack();
   var outbox = new Stack();
 
-  this.enqueue = function() {};
+  this.enqueue = function (val) {
+    inbox.push(val)
+    outbox.push(val)
+    return outbox.reverse()
+  };
 
-  this.dequeue = function() {};
+  this.dequeue = function () {
+    this.outbox.pop()
+  };
 
-  this.size = function() {};
+  this.size = function () {
+    return this.outbox.size();
+  };
+  this.container = function () {
+    return outbox.storage()
+  }
 };
+
+var x = new Queue()
+x.enqueue(1)
+x.enqueue(2)
+x.enqueue(3)
+x.dequeue()
+console.log(x.container())
+
+// console.log(x.size())
+// console.log(x.dequeue())
+// console.log(x.storage())

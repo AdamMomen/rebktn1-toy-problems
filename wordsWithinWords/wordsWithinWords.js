9a993f7a76336d3cffd957908804d7da48e9c754
AdamMomen <adammuman81@gmail.com>
2020-02-18 09:32:40 +0100

    in progress

--- wordsWithinWords/wordsWithinWords.js
+++ wordsWithinWords/wordsWithinWords.js
@@ -7,6 +7,38 @@ nestedWordCount([ "gray", "grays", "ray", "rays", "strays" ]) // 'grays'
 
 */
 
+// input: array 
+// Output: string 
+// Edge case: none
+// Constraints: none
 function nestedWordCount(words) {
-  // your code here...
+  // Remember Adam dont complicate things
+  // declare  an object
+
+  let wordsObj = {};
+  // iterate  i => words  
+  for (let word of words) {
+    // iterate   j=> words
+    for (let word1 of words) {
+      // check if a word is including on a word
+      if (word1.includes(word) && word !== word1) {
+        // check if it's stored on the object
+        if (wordsObj[word]) {
+          wordsObj[word]++
+          // create it 
+        } else {
+          wordsObj[word] = 1
+        }
+      }
+    }
+  }
+  for (let key in wordsObj) {
+    var result = 0;
+    console.log(key)
+    if (key.length > result) {
+      result = key
+    }
+  }
+  return result
 }
+console.log(nestedWordCount(["gray", "grays", "ray", "rays", "strays"]))

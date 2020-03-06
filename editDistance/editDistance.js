7d21ce97c607633397b71b307dc6fa481c951852
AdamMomen <adammuman81@gmail.com>
2020-03-06 15:41:10 +0100

    solved w/ dynamic programming)

--- editDistance/editDistance.js
+++ editDistance/editDistance.js
@@ -1,19 +1,57 @@
 /*
 Edit Distance
-Given two strings, find the minimum number of edits/operations required to convert the first string into the second string, given that the only operations can be insertion, deletion, or substitution.
+Given two strings, find the minimum number of edits/operations required to convert the first string into the second string,
+given that the only operations can be insertion, deletion, or substitution.
 Challenge: Do this in O(m x n) time, where m and n are the respective lengths of str1 and str2.
 
 
 
 editDistance("cat", "cut") // 1
-
 editDistance("wednesday", "sunday") // 5
-
+/
+// remove wed  -> nesday
+// change e => u  nesday => nusday
+// repace s with n sundat
 editDistance("hackerrank", "hackreactor") // 6
 
 For more information check out this article: https://en.wikipedia.org/wiki/Levenshtein_distance
 */
 
 function editDistance(str1, str2) {
-    // your code here...
+  // if  one of the strings is empty
+  // return the others's length
+  // fill the first columan and row with same indices
+  // if the  chars  @ i and j equals to eachotehr
+  // push index @ i-1 & j-1
+  // fill the how are
+
+  // big O(n^2)
+  var board = [],
+    i,
+    j;
+  if (!str1.length) return str2.length;
+  if (!str2.length) return str1.length;
+
+  for (i = 0; i < str1.length; i++) {
+    board[i] = [i];
+  }
+  for (j = 0; j < str2.length; j++) {
+    board[0][j] = j;
+  }
+  for (i = 1; i < str1.length; i++) {
+    for (j = 1; j < str2.length; j++) {
+      if (str2.charAt(i - 1) == str1.charAt(j - 1)) {
+        board[i][j] = board[i - 1][j - 1];
+      } else {
+        board[i][j] = Math.min(
+          board[i - 1][j - 1] + 1, // diagonal
+          board[i][j - 1] + 1, // left
+          board[i - 1][j] + 1 // right
+        );
+      }
+    }
+  }
+  return board[str1.length - 1][str2.length - 1];
 }
+
+console.log(editDistance("sunday", "wednesday"));
